1f4b44694ecfe9e0f126f8e37cd2a07013808302
Douglas Morrow <douglas.p.morrow@gmail.com>
2016-05-28 19:19:26 -0700

    Use let and const instead of var

--- test/test.ts
+++ test/test.ts
@@ -2,8 +2,18 @@ import test = require('blue-tape');
 import ndarray = require('ndarray');
 
 test('module can load properly', null, function(t) {
-  var test = ndarray([]);
+  let test = ndarray([]);
   t.notEqual(null, test, 'created instance is not null');
   t.notEqual(undefined, test.set, 'has expected method: set ');
+  t.end();
+});
+
+test('can get a value that was set', null, function(t) {
+  let test = ndarray([], [1,1]);
+  const value = 34;
+
+  test.set(0,0, value);
+
+  t.equal(test.get(0,0), value);
   t.end();
 });
